c7400360f33a77fbb41f45bd2f291e0b13ad1084
IsypMykhailo <amazonkanameforme@gmail.com>
2022-07-05 17:02:25 +0100

    composer

--- 05_07/index.php
+++ 05_07/index.php
@@ -1,16 +1,15 @@
 <pre>
 <?php
+require_once ("vendor/autoload.php");
 
 use \Mike\Class1 as Mike;
 use \Pv021\Class1 as Pv;
 
-require_once ("autoload.php");
-
-$v = new \Mike\Class1();
-echo $v;
-
-$v2 = new \Pv021\Class1();
-echo $v2;
+//$v = new \Mike\Class1();
+//echo $v;
+//
+//$v2 = new \Pv021\Class1();
+//echo $v2;
 
 $v3 = new Mike();
 echo $v3;
