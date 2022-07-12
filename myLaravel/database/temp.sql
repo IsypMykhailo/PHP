f201b1af5b68e5143614d436773c8af2827a79d2
IsypMykhailo <amazonkanameforme@gmail.com>
2022-07-12 17:56:32 +0100

    myLaravel db

---
+++ myLaravel/database/temp.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `pv021`.`users`
+(
+    `id` BIGINT NOT NULL AUTO_INCREMENT ,
+    `email` VARCHAR(255) NOT NULL ,
+    `password` VARCHAR(255) NOT NULL ,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
