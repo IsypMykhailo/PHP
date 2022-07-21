ff8b97c5e84f0ba84b16addd3cfabf93ba19b643
IsypMykhailo <amazonkanameforme@gmail.com>
2022-07-21 18:38:35 +0200

    migrations

--- lara/src/routes/web.php
+++ lara/src/routes/web.php
@@ -30,3 +30,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/entity', [App\Http\Controllers\EntityController::class, 'readAll'])->name('entity.read.all');
