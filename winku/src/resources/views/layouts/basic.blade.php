07252d58145942d26196ab30cfaa1cc227501a35
IsypMykhailo <amazonkanameforme@gmail.com>
2022-07-30 21:44:17 +0200

    winku

---
+++ winku/src/resources/views/layouts/basic.blade.php
@@ -0,0 +1,271 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>Winku Social Network Toolkit</title>
+    <link rel="icon" href="{{asset('images/fav.png')}}" type="image/png" sizes="16x16">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <link rel="stylesheet" href="{{asset('css/main.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/color.css')}}">
+    <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
+
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">-->
+
+    <!-- Styles -->
+    <!--<link href="{{ asset('css/app.css') }}" rel="stylesheet">-->
+
+    <!-- Scripts -->
+    <!--@vite(['resources/sass/app.scss', 'resources/js/app.js'])-->
+</head>
+<body>
+<div id="app">
+    <!--<div class="se-pre-con"></div>-->
+    <div class="theme-layout">
+
+        <div class="responsive-header">
+            <div class="mh-head first Sticky">
+			<span class="mh-btns-left">
+				<a class="" href="#menu"><i class="fa fa-align-justify"></i></a>
+			</span>
+                <span class="mh-text">
+				<a href="newsfeed.html" title=""><img src="{{asset('images/logo2.png')}}" alt=""></a>
+			</span>
+                <span class="mh-btns-right">
+				<a class="fa fa-sliders" href="#shoppingbag"></a>
+			</span>
+            </div>
+            <div class="mh-head second">
+                <form class="mh-form">
+                    <input placeholder="search" />
+                    <a href="#/" class="fa fa-search"></a>
+                </form>
+            </div>
+
+        </div><!-- responsive header -->
+
+        <div class="topbar stick">
+            <div class="logo">
+                <a title="" href="{{url('/home')}}"><img src="{{asset('images/logo.png')}}" alt=""></a>
+            </div>
+
+            <div class="top-area">
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ms-auto py-2">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+                <!--<div class="user-img">
+                    <img src="{{asset('images/resources/admin.jpg')}}" alt="">
+                    <span class="status f-online"></span>
+                    <div class="user-setting">
+                        <a href="#" title=""><span class="status f-online"></span>online</a>
+                        <a href="#" title=""><span class="status f-away"></span>away</a>
+                        <a href="#" title=""><span class="status f-off"></span>offline</a>
+                        <a href="#" title=""><i class="ti-user"></i> view profile</a>
+                        <a href="#" title=""><i class="ti-pencil-alt"></i>edit profile</a>
+                        <a href="#" title=""><i class="ti-target"></i>activity log</a>
+                        <a href="#" title=""><i class="ti-settings"></i>account setting</a>
+                        <a href="#" title=""><i class="ti-power-off"></i>log out</a>
+                    </div>
+                </div>
+                <span class="ti-menu main-menu" data-ripple=""></span>-->
+            </div>
+        </div><!-- topbar -->
+
+        <main>
+            @yield('content')
+        </main>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4">
+                        <div class="widget">
+                            <div class="foot-logo">
+                                <div class="logo">
+                                    <a href="index-2.html" title=""><img src="{{asset('images/logo.png')}}" alt=""></a>
+                                </div>
+                                <p>
+                                    The trio took this simple idea and built it into the world’s leading carpooling platform.
+                                </p>
+                            </div>
+                            <ul class="location">
+                                <li>
+                                    <i class="ti-map-alt"></i>
+                                    <p>33 new montgomery st.750 san francisco, CA USA 94105.</p>
+                                </li>
+                                <li>
+                                    <i class="ti-mobile"></i>
+                                    <p>+1-56-346 345</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4">
+                        <div class="widget">
+                            <div class="widget-title"><h4>follow</h4></div>
+                            <ul class="list-style">
+                                <li><i class="fa fa-facebook-square"></i> <a href="https://web.facebook.com/shopcircut/" title="">facebook</a></li>
+                                <li><i class="fa fa-twitter-square"></i><a href="https://twitter.com/login?lang=en" title="">twitter</a></li>
+                                <li><i class="fa fa-instagram"></i><a href="https://www.instagram.com/?hl=en" title="">instagram</a></li>
+                                <li><i class="fa fa-google-plus-square"></i> <a href="https://plus.google.com/discover" title="">Google+</a></li>
+                                <li><i class="fa fa-pinterest-square"></i> <a href="https://www.pinterest.com/" title="">Pintrest</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4">
+                        <div class="widget">
+                            <div class="widget-title"><h4>Navigate</h4></div>
+                            <ul class="list-style">
+                                <li><a href="about.html" title="">about us</a></li>
+                                <li><a href="contact.html" title="">contact us</a></li>
+                                <li><a href="terms.html" title="">terms & Conditions</a></li>
+                                <li><a href="#" title="">RSS syndication</a></li>
+                                <li><a href="sitemap.html" title="">Sitemap</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4">
+                        <div class="widget">
+                            <div class="widget-title"><h4>useful links</h4></div>
+                            <ul class="list-style">
+                                <li><a href="#" title="">leasing</a></li>
+                                <li><a href="#" title="">submit route</a></li>
+                                <li><a href="#" title="">how does it work?</a></li>
+                                <li><a href="#" title="">agent listings</a></li>
+                                <li><a href="#" title="">view All</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4">
+                        <div class="widget">
+                            <div class="widget-title"><h4>download apps</h4></div>
+                            <ul class="colla-apps">
+                                <li><a href="https://play.google.com/store?hl=en" title=""><i class="fa fa-android"></i>android</a></li>
+                                <li><a href="https://www.apple.com/lae/ios/app-store/" title=""><i class="ti-apple"></i>iPhone</a></li>
+                                <li><a href="https://www.microsoft.com/store/apps" title=""><i class="fa fa-windows"></i>Windows</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer><!-- footer -->
+        <div class="bottombar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <span class="copyright"><a target="_blank" href="https://www.templateshub.net">Templates Hub</a></span>
+                        <i><img src="{{asset('images/credit-cards.png')}}" alt=""></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="side-panel">
+        <h4 class="panel-title">General Setting</h4>
+        <form method="post">
+            <div class="setting-row">
+                <span>use night mode</span>
+                <input type="checkbox" id="nightmode1"/>
+                <label for="nightmode1" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>Notifications</span>
+                <input type="checkbox" id="switch22" />
+                <label for="switch22" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>Notification sound</span>
+                <input type="checkbox" id="switch33" />
+                <label for="switch33" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>My profile</span>
+                <input type="checkbox" id="switch44" />
+                <label for="switch44" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>Show profile</span>
+                <input type="checkbox" id="switch55" />
+                <label for="switch55" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+        </form>
+        <h4 class="panel-title">Account Setting</h4>
+        <form method="post">
+            <div class="setting-row">
+                <span>Sub users</span>
+                <input type="checkbox" id="switch66" />
+                <label for="switch66" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>personal account</span>
+                <input type="checkbox" id="switch77" />
+                <label for="switch77" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>Business account</span>
+                <input type="checkbox" id="switch88" />
+                <label for="switch88" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>Show me online</span>
+                <input type="checkbox" id="switch99" />
+                <label for="switch99" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>Delete history</span>
+                <input type="checkbox" id="switch101" />
+                <label for="switch101" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+            <div class="setting-row">
+                <span>Expose author name</span>
+                <input type="checkbox" id="switch111" />
+                <label for="switch111" data-on-label="ON" data-off-label="OFF"></label>
+            </div>
+        </form>
+    </div><!-- side panel -->
+
+    <script data-cfasync="false" src="{{asset('../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js')}}"></script><script src="js/main.min.js"></script>
+    <script src="{{asset('js/script.js')}}"></script>
+    <script src="{{asset('js/map-init.js')}}"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8c55_YHLvDHGACkQscgbGLtLRdxBDCfI"></script>
+</div>
+</body>
+</html>
